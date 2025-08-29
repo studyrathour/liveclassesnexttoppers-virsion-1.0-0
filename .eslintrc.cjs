a3349612d41fd75637a4c7f55639d4b7432adde6
studyrathour <rathourravishankar186@gmail.com>
2025-08-29 08:25:04 +0530

    Commit from Dualite Alpha:

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    'postcss.config.js',
+    'tailwind.config.js',
+    'vite.config.js',
+    'eslint.config.js'
+  ],
+};
